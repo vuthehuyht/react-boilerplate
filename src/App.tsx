7c081e117d8ac957fc2c81ed7db6f0917942b76f
Vu The Huy <vuthehuy.t61@gmail.com>
2024-04-13 18:20:20 +0700

    change name

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import './assets/styles/app.css'
 import './assets/styles/app.scss'
 
 const App: FC = () => {
-  const [fullname] = useState('Dư Thanh Được')
+  const [fullname] = useState('Vũ Thế Huy')
   console.log(fullname)
   return (
     <div>
       <img src={reactlogo} alt='React Logo' width={100} height={100} />
       <h1>{fullname}</h1>
-      <h2>Bài viết được viết tại blog {process.env.HOST}</h2>
+      <h2>Bài viết được viết tại blog AWS Cloud Journey</h2>
     </div>
   )
 }
